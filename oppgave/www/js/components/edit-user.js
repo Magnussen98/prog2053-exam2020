598d95ef16dae7789caeb3c687a4b1243012f35a
Andreas Magnussen <andreasmagnussen@Andreass-MacBook-Pro.local>
2020-12-17 11:42:47 +0100

    displayUser()

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -4,34 +4,68 @@ class EditUser extends LitElement {
   static get properties() {
     return {
       user: { type: Object },
-      checkForUser: { type: Function }
+      displayUser: { type: String },
+      changePwd: { type: String }
     };
   }
 
   constructor(){
     super();
-    this.checkForUser();
+    this.displayUser = "none"
+    this.changePwd ="noChange"
   }
+
+  checkForUser(){
+    if (this.user.uid)
+      this.displayUser = "block"
+  }
+
+    
 
   render(){
     return html `
-    <h1 id="${console.log(this.user)}">Delete</h1>
-    <form>
-      <div class="email">
-        <label for="email">Email</label>
-        <input type="email" id="uname" class="uname" value="${this.user.uname}">
-      </div>
-      <div class="firstName">
-        <label for="firstName">First name</label>
-        <input type="text" id="firstName" class="firstName" value="${this.user.firstName}">
-      </div>
-      <div class="lastName">
-        <label for="lastName">Last name</label>
-        <input type="text" id="lastName" class="lastName" value="${this.user.lastName}">
-      </div>
-    </form>
+    <head>
+      <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    </head>
+    <div class="container ${this.checkForUser()}" style="display: ${this.displayUser}">
+      <form class="userForm" method="POST" style="width: 40%">
+          <div class="email">
+            <label for="email">Email</label>
+            <input type="email" id="uname" class="uname" value="${this.user.uname}">
+            <input type="hidden" id="uid" class="uid" value="${this.user.uid}">
+          </div>
+          <div class="firstName">
+            <label for="firstName">First name</label>
+            <input type="text" id="firstName" class="firstName" value="${this.user.firstName}">
+          </div>
+          <div class="lastName">
+            <label for="lastName">Last name</label>
+            <input type="text" id="lastName" class="lastName" value="${this.user.lastName}">
+          </div>
+        <!--  <button class="change-password btn btn-sm btn-primary mt-2" id="changePassword" @onclick="${this.changePwd = 'block'}" style="display">Change password</button>   -->
+          <div class="oldpwd">
+            <label for="oldpwd">Old password</label>
+            <input type="text" id="oldpwd" class="oldpwd" value="">
+          </div>
+          <div class="pwd">
+            <label for="pwd">New password</label>
+            <input type="password" id="pwd" class="pwd" value="">
+          </div>
+          <div class="confPwd">
+            <label for="confPwd">Confirm password</label>
+            <input type="password" id="confPwd" class="confPwd" value="">
+          </div>
+          
+          
+      </form>
+    </div>
+
     `
   }
+
+  
 
 }
 customElements.define('edit-user', EditUser);
