8468ac6a3686e74e596388ed2bf24f9496bfa06f
Andreas Magnussen <andreasmagnussen@Andreass-MacBook-Pro.local>
2020-12-17 10:56:52 +0100

    Startet

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -3,11 +3,35 @@ import { LitElement, html, css } from "../../node_modules/lit-element/lit-elemen
 class EditUser extends LitElement {
   static get properties() {
     return {
-      user: { type: Object }
+      user: { type: Object },
+      checkForUser: { type: Function }
     };
   }
 
-  // din kode her
+  constructor(){
+    super();
+    this.checkForUser();
+  }
+
+  render(){
+    return html `
+    <h1 id="${console.log(this.user)}">Delete</h1>
+    <form>
+      <div class="email">
+        <label for="email">Email</label>
+        <input type="email" id="uname" class="uname" value="${this.user.uname}">
+      </div>
+      <div class="firstName">
+        <label for="firstName">First name</label>
+        <input type="text" id="firstName" class="firstName" value="${this.user.firstName}">
+      </div>
+      <div class="lastName">
+        <label for="lastName">Last name</label>
+        <input type="text" id="lastName" class="lastName" value="${this.user.lastName}">
+      </div>
+    </form>
+    `
+  }
 
 }
 customElements.define('edit-user', EditUser);
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
+Candidate: 10030
+
+
 # PROG2053 eksamen v2020
 * Lag en FORK av dette repositoriet og gi meg leserettigheter (aland.a.mendoza@ntnu.no). Det som er i repositoriet på slutten av eksamen teller som din besvarelse.
 * Legg inn kandidatnummeret ditt øverst i readme.md fila.
